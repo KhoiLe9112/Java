09d25fd661f743119dc6d1d100ebf9f5a90d1b8d
USER <USER@LAPTOP-T9VQVJPJ>
2021-09-15 15:00:43 +0700

    area

---
+++ src/main/java/math.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class math {
+    public static void main(String[] args) {
+        System.out.println("What is 9 - 2?");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the question: ");
+        int x = input.nextInt();
+        if (x == 6) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
