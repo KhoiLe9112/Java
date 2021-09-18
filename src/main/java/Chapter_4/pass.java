17e64b7065622ca9b782b9f85f18becdf962c5c1
KhoiLe9112 <79627220+KhoiLe9112@users.noreply.github.com>
2021-09-18 20:33:18 +0700

    Update pass.java

--- src/main/java/Chapter_4/pass.java
+++ src/main/java/Chapter_4/pass.java
@@ -8,23 +8,7 @@
  *
  * @author USER
  */
-/*import java.util.Scanner;
-public class pass {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        char username;
-        int password;
-        System.out.println("Username: ");
-        username = (char) input.nextInt();
-        System.out.println("Password: ");
-        password = input.nextInt();
-        if ((username == "HuyHoang") && password == 2021) {
-            System.out.println("Ban da nhap sai username va password ");}
-        else {
-            System.out.println("Ban da nhap dung username va password ");}
-        
-    }
-}*/
+
 
 package Chapter_4;
 
